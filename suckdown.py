95c919bed4b7875bea26d5c60bc0dff420269395
Dan Bailey <dbailey@danbailey.net>
2023-06-09 05:02:07 -0500

    first commit

---
+++ suckdown.py
@@ -0,0 +1,9 @@
+# library loading
+
+# this could be a .env but for now, it's not
+
+# connect to reddit
+
+# suck down everything because fuck those guys
+
+# store the stuff elsewhere (mongodb in this case)
